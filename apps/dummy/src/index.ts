72b4c4490a96270aae42c69c5101c73f3b187622
joao <jpgmoreira19@gmail.com>
2026-01-01 16:14:09 -0300

    still configuring, creating dummy app

---
+++ apps/dummy/src/index.ts
@@ -0,0 +1,4 @@
+import { SHARED_NAME, sharedHello } from "@shared/root";
+
+console.log(SHARED_NAME);
+console.log(sharedHello("dummy-app"));
---
+++ shared/src/index.ts
@@ -0,0 +1,5 @@
+export const SHARED_NAME = "shared-root";
+
+export function sharedHello(name: string) {
+  return `Hello ${name} from shared`;
+}
