fd1ed681d80f7cf47eecaf290cb5253c13076dff
joao <jpgmoreira19@gmail.com>
2026-01-03 15:57:24 -0300

    put modal to interapp

--- interapp/src/components/Modal.vue
+++ interapp/src/components/Modal.vue
@@ -5,16 +5,11 @@
       type: Boolean,
       required: true,
     },
-    frozen: {
-      type: Boolean,
-      required: false,
-    },
   });
   const emit = defineEmits<{
     (e: 'close'): void;
   }>();
   function close() {
-    if (props.frozen) return;
     emit('close');
   }
   function onKey(e: KeyboardEvent) {
@@ -32,31 +27,27 @@
 <template>
   <teleport to="body">
     <div class="modal-container">
-      <Transition name="backdrop-fade">
-        <div v-if="props.visible" class="backdrop" @click="close"></div>
-      </Transition>
-      <Transition name="modal-slide">
-        <div v-if="props.visible" class="modal">
-          <div class="modal-header">
-            <div>
-              <slot name="header"></slot>
-            </div>
-            <div class="modal-close" @click="close"></div>
+      <div v-if="props.visible" class="modal-backdrop" @click="close"></div>
+      <div v-if="props.visible" class="modal">
+        <div class="modal-header">
+          <div>
+            <slot name="header"></slot>
           </div>
-          <div class="modal-body">
-            <slot name="body"></slot>
-          </div>
-          <div class="modal-footer">
-            <slot name="footer"></slot>
-          </div>
+          <div class="modal-close" @click="close"></div>
         </div>
-      </Transition>
+        <div class="modal-body">
+          <slot name="body"></slot>
+        </div>
+        <div class="modal-footer">
+          <slot name="footer"></slot>
+        </div>
+      </div>
     </div>
   </teleport>
 </template>
 
 <style scoped>
-  .backdrop {
+  .modal-backdrop {
     position: fixed;
     inset: 0;
     z-index: 9999;
@@ -94,41 +85,5 @@
   }
   .modal-close:hover {
     background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='lightgray' viewBox='0 0 16 16'%3E%3Cpath fill-rule='evenodd' d='M13.854 2.146a.5.5 0 0 1 0 .708l-11 11a.5.5 0 0 1-.708-.708l11-11a.5.5 0 0 1 .708 0Z'/%3E%3Cpath fill-rule='evenodd' d='M2.146 2.146a.5.5 0 0 0 0 .708l11 11a.5.5 0 0 0 .708-.708l-11-11a.5.5 0 0 0-.708 0Z'/%3E%3C/svg%3E");
-  }
-</style>
-
-<style scoped>
-  .backdrop-fade-enter-from,
-  .backdrop-fade-leave-to {
-    opacity: 0;
-  }
-  .backdrop-fade-enter-to,
-  .backdrop-fade-leave-from {
-    opacity: 1;
-  }
-  .backdrop-fade-enter-active,
-  .backdrop-fade-leave-active {
-    transition: opacity 0.3s ease;
-  }
-
-  .modal-slide-enter-from {
-    transform: translate(-50%, calc(-50% - 50px));
-    opacity: 0;
-  }
-  .modal-slide-enter-to {
-    transform: translate(-50%, -50%);
-    opacity: 1;
-  }
-  .modal-slide-leave-from {
-    transform: translate(-50%, -50%);
-    opacity: 1;
-  }
-  .modal-slide-leave-to {
-    transform: translate(-50%, calc(-50% - 50px));
-    opacity: 0;
-  }
-  .modal-slide-enter-active,
-  .modal-slide-leave-active {
-    transition: all 0.3s ease;
   }
 </style>
