9d028b9b5a049f39c37f88c1d4cbf0f21d16ed8d
joao <jpgmoreira19@gmail.com>
2026-01-10 11:29:45 -0300

    .

--- interapp/src/components/Explorer/renderer/Explorer.vue
+++ interapp/src/components/Explorer/renderer/Explorer.vue
@@ -446,7 +446,7 @@
   <div
     v-if="hasLoaded"
     ref="explorer"
-    class="explorer relative h-screen"
+    class="explorer relative h-full"
     @click.right="(e) => showContextMenu('root', null, e)"
     @click="() => (contextState.visible = false)"
     @mouseenter="containerMouseEnter"
