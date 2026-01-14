1ab3eeeb220089f1d233495c4d75a534a5bbca67
joao <jpgmoreira19@gmail.com>
2026-01-14 15:48:33 -0300

    common cleared

--- apps/codegym/src/common/constants.ts
+++ apps/codegym/src/common/constants.ts
@@ -4,7 +4,3 @@ export const APP_NAME = packageJson.name;
 export const APP_PRODUCT_NAME = packageJson.productName;
 export const APP_VERSION = packageJson.version;
 export const APP_HOMEPAGE = packageJson.homepage;
-
-export const POPULARITY_GROUP_SIZE = 20;
-export const HISTORY_PAGE_SIZE = 100;
-export const HISTORY_MAX_SIZE_PER_OJ = 2000;
