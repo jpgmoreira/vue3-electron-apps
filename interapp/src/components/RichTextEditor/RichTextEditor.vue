b5e307a1071131f9d300d0f4b4adce0da2c18b60
joao <jpgmoreira19@gmail.com>
2026-01-21 18:34:12 -0300

    separated context menu into a composable

--- interapp/src/components/RichTextEditor/RichTextEditor.vue
+++ interapp/src/components/RichTextEditor/RichTextEditor.vue
@@ -1,36 +1,9 @@
 <script lang="ts" setup>
-  import { reactive, computed, useTemplateRef, nextTick } from 'vue';
+  import { useTemplateRef } from 'vue';
+  import { useContextMenu } from './useContextMenu';
   const contextRef = useTemplateRef('context-menu');
-  const context = reactive({
-    visible: false,
-    top: 0,
-    left: 0,
-  });
-  const contextStyle = computed(() => ({
-    top: `${context.top}px`,
-    left: `${context.left}px`,
-  }));
-  function openContext(e: MouseEvent) {
-    context.visible = true;
-    nextTick(() => {
-      const ctx = contextRef.value;
-      if (!ctx) throw new Error('Context menu not set!');
-      const box = ctx.getBoundingClientRect();
-      const winWidth = window.innerWidth;
-      const winHeight = window.innerHeight;
-      context.top = Math.min(e.clientY, winHeight - box.height);
-      context.left = Math.min(e.clientX, winWidth - box.width);
-    });
-  }
-  function hideContext() {
-    context.visible = false;
-  }
-  function contextCut() {
-    document.execCommand('cut');
-  }
-  function contextCopy() {
-    document.execCommand('copy');
-  }
+  const { context, hideContext, contextStyle, contextCut, contextCopy, contextPaste, openContext } =
+    useContextMenu(contextRef);
 </script>
 
 <template>
@@ -38,7 +11,7 @@
     <div v-if="context.visible" ref="context-menu" :style="contextStyle" class="context-menu">
       <div @click="contextCut">Cut</div>
       <div @click="contextCopy">Copy</div>
-      <div>Paste</div>
+      <div @click="contextPaste">Paste</div>
     </div>
     <div
       class="editor"
---
+++ interapp/src/components/RichTextEditor/useContextMenu.ts
@@ -0,0 +1,44 @@
+import { reactive, computed, nextTick, Ref } from 'vue';
+
+export function useContextMenu(contextRef: Ref<HTMLElement | null>) {
+  const context = reactive({
+    visible: false,
+    top: 0,
+    left: 0,
+  });
+  const contextStyle = computed(() => ({
+    top: `${context.top}px`,
+    left: `${context.left}px`,
+  }));
+  function openContext(e: MouseEvent) {
+    context.visible = true;
+    nextTick(() => {
+      const ctx = contextRef.value;
+      if (!ctx) throw new Error('Context menu not set!');
+      const box = ctx.getBoundingClientRect();
+      const winWidth = window.innerWidth;
+      const winHeight = window.innerHeight;
+      context.top = Math.min(e.clientY, winHeight - box.height);
+      context.left = Math.min(e.clientX, winWidth - box.width);
+    });
+  }
+  function hideContext() {
+    context.visible = false;
+  }
+  function contextCut() {
+    document.execCommand('cut');
+  }
+  function contextCopy() {
+    document.execCommand('copy');
+  }
+  function contextPaste() {}
+  return {
+    context,
+    contextStyle,
+    openContext,
+    hideContext,
+    contextCut,
+    contextCopy,
+    contextPaste,
+  };
+}
